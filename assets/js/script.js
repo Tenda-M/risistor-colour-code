ad5da4fb35855d27a1c42602332f0a11f116bcf6
Tatenda Mudehwe <tmudehwe@hotmail.com>
2024-04-03 18:56:55 +0000

    Add basic JS add functionality to dropdown button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,17 @@
 const dropdownBtn = document.querySelector(".dropdown__button");
 const dropdownBand = document.querySelector(".dropdown__band");
 
-//functon to get dropdown menu
+//add event listerner to get dropdown menu
 dropdownBtn.addEventListener("click",() => {
     //toggle the buttone to hide and open 
     dropdownBand.classList.toggle("hide");
 });
+
+// add even listner that will allow dropdown to close when not in use
+window.addEventListener('click', (e) =>{
+//if target is not equal to then hide button dropdown
+if(e.target!==dropdownBtn){
+dropdownBand.classList.add("hide");
+}
+});
+
