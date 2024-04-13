a9d1abaa605afede52be14504f6f0fd0aff6c815
Tatenda Mudehwe <tmudehwe@hotmail.com>
2024-04-13 20:26:07 +0000

    Add strcuture for resistor colour image and css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,19 @@ let Minimum = 0;
 let Maximum = 0;
 
 // Function to check the radio button and show/hide select elements accordingly
-function check(){
+function check() {
   // if 4-band-button radio button is checked
-  if(document.getElementById("4-band-button").checked){
-    document.getElementById("select-three").hidden = true; 
+  if (document.getElementById("4-band-button").checked) {
+    document.getElementById("select-three").hidden = true;
     document.getElementById("select-four").hidden = true;
   }
   // if 5-band-button radio button is checked
-  if(document.getElementById("5-band-button").checked == true){
+  if (document.getElementById("5-band-button").checked == true) {
     document.getElementById("select-three").hidden = false;
     document.getElementById("select-four").hidden = true;
   }
   // if 6-band-button radio button is checked
-  if(document.getElementById("6-band-button").checked){
+  if (document.getElementById("6-band-button").checked) {
     document.getElementById("select-three").hidden = false;
     document.getElementById("select-four").hidden = false;
   }
@@ -33,36 +33,36 @@ function calculateAnswer() {
   let selectFive = document.getElementById('select-five').value;
 
   // Formula for All the bands selection,// Converts numbers to strings and concatenates them
-  if (document.getElementById("4-band-button").checked == true){
+  if (document.getElementById("4-band-button").checked == true) {
     Result = (parseInt(selectOne).toString() + parseInt(selectTwo).toString()) * parseInt(selectFive);
-  } 
-  if (document.getElementById("5-band-button").checked == true){
+  }
+  if (document.getElementById("5-band-button").checked == true) {
     Result = (parseInt(selectOne).toString() + parseInt(selectTwo).toString() + parseInt(selectThree).toString()) * parseInt(selectFive);
   }
-  if (document.getElementById("6-band-button").checked == true){
+  if (document.getElementById("6-band-button").checked == true) {
     Result = (parseInt(selectOne).toString() + parseInt(selectTwo).toString() + parseInt(selectThree).toString() + parseInt(selectFour).toString()) * parseInt(selectFive);
-  }  
+  }
 }
 
 // tolerance calculations
-function calculateTolerance(){
-  if (document.getElementById("select-tolerance").value === "Gold"){
+function calculateTolerance() {
+  if (document.getElementById("select-tolerance").value === "Gold") {
     Tolerance = Result * 0.05;
     Minimum = Result - Tolerance;
     Maximum = Result + Tolerance;
-  } 
-  else if (document.getElementById("select-tolerance").value === "Silver"){
+  }
+  else if (document.getElementById("select-tolerance").value === "Silver") {
     Tolerance = Result * 0.1;
     Minimum = Result - Tolerance;
     Maximum = Result + Tolerance;
   }
-   
-  else{
+
+  else {
     Tolerance = 0; // Default value if no tolerance is selected
     Minimum = Result;
     Maximum = Result;
   }
- 
+
 }
 
 // Function to display the calculated answer
@@ -80,16 +80,23 @@ function showCalculatedAnswer() {
 
 }
 
-/// Page popup
+//******* Page popup box **********
 // an event listener to the element that will trigger the popup
 document.querySelector(".popup").style.display = "none";
-document.getElementById("popupTrigger").addEventListener("click", function() {
-document.querySelector(".popup").style.display = "block";
+document.getElementById("popupTrigger").addEventListener("click", function () {
+  // Show overlay
+  //document.getElementById("overlay").style.display = "block";
+  // Show popup
+  document.querySelector(".popup").style.display = "block";
 });
-
-
 //function adds an event listener to the close button. When the close button is clicked, it hides the popup window by setting its display property to "none".
-  document.querySelector("#close").addEventListener("click", function(){
+document.querySelector("#close").addEventListener("click", function () {
+  // Hide overlay
+  //document.getElementById("overlay").style.display = "none";
+  // Hide popup
   document.querySelector(".popup").style.display = "none";
 })
+
+//Resistor image
+
 
